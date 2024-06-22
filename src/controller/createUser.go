5383f0bed922525f550ed65affb4b1eab4daf3a0
murillolamego <murillolamegolopes@live.com>
2024-06-21 21:56:57 -0300

    first request and response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/murillolamego/golang-basic/src/config/rest_err"
+	"github.com/murillolamego/golang-basic/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
